8e8198db75ebd06f9a7a61cd455ceae61f286178
AlainNgauthier <alain.ndamwey@estudante.ufscar.br>
2020-12-21 03:55:36 -0300

    container content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import marked from 'marked';
+import { sampleText } from './sampleText';
+
+class App extends Component {  
+  state = {
+    text: sampleText
+  }
+
+componentDidMount(){
+  const text = localStorage.getItem('textUpdated');
+  
+  if(text){
+    this.setState({ text })
+  }else{
+    this.setState({ text: sampleText })
+    /* Si le champ de text est vide, à l'actualisation
+     * de la page, elle est renderisée avec le texte sampleText
+     */
+  }
+}
+
+componentDidUpdate(){
+  const { text } = this.state;
+  localStorage.setItem('textUpdated', text)
+}
+
+handleChange = event => {
+  const text = event.target.value;
+  this.setState({ text })
+}  
+
+renderText = text => {
+  const __html = marked(text, { sanitize: true });
+  return{ __html }
+}
+
+  
+render(){
+    return (
+      <>
+        <div className="container">
+          <div className="row" >
+              <div className="col-sm-6">
+                <textarea
+                    onChange={this.handleChange}
+                    value={ this.state.text } 
+                    rows="25" 
+                    className='form-control'>
+                </textarea>
+              </div>
+              <div className="col-sm-6">
+                <div>
+                  <div dangerouslySetInnerHTML={this.renderText(this.state.text)}/>
+                </div>
+              </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/sampleText.js
@@ -0,0 +1,1 @@
+export const sampleText = '# Titre \n## Sous-Titre \n### Titre plus profond\n\nLes paragraphes sont separés\npar une ligne vide.\n\nLaissé deux espaces à la fin d\'une ligne pour \naller à la ligne.\n\nAttributs *italique*, **gras**, ~~rayé~~.\n\nListe:\n\n * pommes\n * oranges\n *poires\n\nListe numérotée:\n\n1.jambon\n2.cornichon\n3. pain\n\n'
